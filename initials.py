64e3700a3ab2a7c06e22fa07359b9683a2497ce5
Ryan Tettey <2425402002@live.gctu.edu.gh>
2026-05-31 13:59:51 -0700

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+#File: initials.py
+#Description: Print out my initials
+#Assignment:Number 2
+#
+#Name: Ryan Elinam Tettey
+#STUDENT ID:2425402002
+#Email:2425402002@live.gctu.edu.gh
+#Grader Augustus Buckman
+#
+#On my honor, I Ryan Elinam Tettey, this programming assignment is my own work
+#and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print initials RET: first small, then large 12x10 block letters using print and strings only.
 
+    print()
+    print("...RET")
+    print()
+    print("..." + "RRRRRRRRRR.." + "..." + ".." + "..." + "EEEEEEEEEEEE" + "..." + ".." + "..." + "TTTTTTTTTTTT" + ".....")
+    print("..." + "RR........RR" + "..." + ".." + "..." + "EE.........." + "..." + ".." + "..." + ".....TT....." + ".....")
+    print("..." + "RR........RR" + "..." + ".." + "..." + "EE.........." + "..." + ".." + "..." + ".....TT....." + ".....")
+    print("..." + "RR........RR" + "..." + ".." + "..." + "EE.........." + "..." + ".." + "..." + ".....TT....." + ".....")
+    print("..." + "RRRRRRRRRR.." + "..." + ".." + "..." + "EEEEEEEEEEEE" + "..." + ".." + "..." + ".....TT....." + ".....")
+    print("..." + "RR.RR......." + "..." + ".." + "..." + "EE.........." + "..." + ".." + "..." + ".....TT....." + ".....")
+    print("..." + "RR...RR....." + "..." + ".." + "..." + "EE.........." + "..." + ".." + "..." + ".....TT....." + ".....")
+    print("..." + "RR.....RR..." + "..." + ".." + "..." + "EE.........." + "..." + ".." + "..." + ".....TT....." + ".....")
+    print("..." + "RR......RR.." + "..." + "**" + "..." + "EE.........." + "..." + "**" + "..." + ".....TT....." + "...**")
+    print("..." + "RR........RR" + "..." + "**" + "..." + "EEEEEEEEEEEE" + "..." + "**" + "..." + ".....TT....." + "...**")
+    print()
 
 
 main()
